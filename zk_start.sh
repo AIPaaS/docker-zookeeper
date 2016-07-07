c84d5a1a323e0163e33781acfe6b4243f76899d7
yuanman <yuanman@asiainfo.com>
2016-07-07 16:18:04 +0800

    added ZK_PORT

--- zk_start.sh
+++ zk_start.sh
@@ -10,5 +10,7 @@ echo "${SERVER_1}" >> ${ZOO_CFG}
 echo "${SERVER_2}" >> ${ZOO_CFG}
 echo "${SERVER_3}" >> ${ZOO_CFG}
 
+sed -i "s/clientPort=2181/clientPort=${ZK_PORT}/g" ${ZOO_CFG}
+
 # Start Zookeeper
 /opt/zookeeper/bin/zkServer.sh start-foreground
